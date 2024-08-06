3b7fae314084e5829b7d9b2f4aa1fcf71c167e64
ayushiparnalia <169115461+Ayushiparnalia@users.noreply.github.com>
2024-08-05 21:59:09 -0700

    Added the function for random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('Hello, Welcome to the game!')
+let options = ['rock', 'paper', 'scissors'];
+function computerChoice() {
+    let computerSelect = options[(Math.floor(Math.random() * options.length))];
+    return computerSelect;
+}
+console.log(computerChoice())
